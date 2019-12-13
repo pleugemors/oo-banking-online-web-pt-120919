c53d3173c73ce69c1411fcce2d21c4c030bfdc41
Paul Leugemors <pleugemors@yahoo.com>
2019-12-13 21:52:12 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,6 +3,7 @@ class Transfer
   def initialize(tranferer, receiver, amount)
     @sender = tranferer
     @receiver = receiver
+    @status = "pending"
   end 
   
   attr_reader :sender, :receiver
