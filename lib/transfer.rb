d8680f1a3c3207c06cf66be5d316cd20b83458df
Paul Leugemors <pleugemors@yahoo.com>
2019-12-13 21:55:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,8 +10,7 @@ class Transfer
   attr_reader :sender, :receiver, :status, :amount 
   
   def valid?
-    (sender.valid? && 
-    
+    (sender.valid? && receiver.valid?) ? true : false 
   end
   
 end
