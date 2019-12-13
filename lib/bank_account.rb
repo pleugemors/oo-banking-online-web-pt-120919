bc8e83b61deba4150328f0f58be0254938253389
Paul Leugemors <pleugemors@yahoo.com>
2019-12-13 21:39:00 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,8 +3,9 @@ class BankAccount
   def initialize(name)
     @name = name 
     @balance = 1000
+    @status = "open"
   end
   
-  attr_reader :name, :balance 
+  attr_reader :name, :balance, :status 
 
 end
