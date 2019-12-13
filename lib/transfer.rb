181c5045f1c51bec98fe52ca58f40a07183dbdea
Paul Leugemors <pleugemors@yahoo.com>
2019-12-13 22:11:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       receiver.deposit(amount)
       @status = "complete"
     else
-      @status = "pending"
+      @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end 
   end
